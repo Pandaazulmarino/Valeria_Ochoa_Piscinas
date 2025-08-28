e481237974e11cd1bec6d8ed2d41bc8fcf6f32db
Vale <ochoa.bedoya.valeria@gmail.com>
2025-08-28 08:56:25 -0500

    creación de clases

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Piscinas
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Profundidad;
+    public bool Activa;
+    public DateTime FechaInaguración;
+    public Estados? Estado;
+    public List<Articulos>? Articulos;
+    public Categorias? Categoria;
+
+}
+
+public class Estados
+{
+    public int Id;
+    public string? Estado;
+}
+
+public class Articulos
+{
+    public int Id;
+    public string? Articulo;
+}
+
+public class Categorias
+{
+    public int id;
+    public string? Categoria;
+}
